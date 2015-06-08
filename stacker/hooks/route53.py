6c86f7ef2ba5acdb387d154a618dd0f6bb65af6c
Michael Barrett <loki77@gmail.com>
2015-06-08 13:49:36 -0700

    Allow creation of domains without BaseDomain
    Gives the hook the ability to parse args.

--- stacker/hooks/route53.py
+++ stacker/hooks/route53.py
@@ -10,10 +10,9 @@ from stacker.util import create_route53_zone
 
 def create_domain(region, namespace, mappings, parameters, **kwargs):
     conn = ConnectionManager(region)
-    try:
-        domain = parameters['BaseDomain']
-    except KeyError:
-        logger.error("BaseDomain parameter not provided.")
+    domain = kwargs.get('domain', parameters.get('BaseDomain'))
+    if not domain:
+        logger.error("domain argument or BaseDomain parameter not provided.")
         return False
     create_route53_zone(conn.route53, domain)
     return True

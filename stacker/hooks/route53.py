d18e0eb8986cd963c70b4c237fcd7d8ca6fa1cf6
Michael Barrett <loki77@gmail.com>
2015-08-16 21:21:54 -0700

    Remove all instances of aws_helper
    Fixes #76

--- stacker/hooks/route53.py
+++ stacker/hooks/route53.py
@@ -2,17 +2,16 @@ import logging
 
 logger = logging.getLogger(__name__)
 
-
-from aws_helper.connection import ConnectionManager
+from boto.route53 import connect_to_region
 
 from stacker.util import create_route53_zone
 
 
 def create_domain(region, namespace, mappings, parameters, **kwargs):
-    conn = ConnectionManager(region)
+    conn = connect_to_region(region)
     domain = kwargs.get('domain', parameters.get('BaseDomain'))
     if not domain:
         logger.error("domain argument or BaseDomain parameter not provided.")
         return False
-    create_route53_zone(conn.route53, domain)
+    create_route53_zone(conn, domain)
     return True

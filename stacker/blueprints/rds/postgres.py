f80b080f62b450531007f58849019fd18c75c25f
Michael Barrett <loki77@gmail.com>
2015-12-01 10:35:39 -0800

    Add new versions of Postgres

--- stacker/blueprints/rds/postgres.py
+++ stacker/blueprints/rds/postgres.py
@@ -6,7 +6,8 @@ class PostgresMixin(object):
         return "postgres"
 
     def get_engine_versions(self):
-        return ['9.3.1', '9.3.2', '9.3.3', '9.3.5', '9.3.6', '9.4.1']
+        return ['9.3.1', '9.3.2', '9.3.3', '9.3.5', '9.3.6', '9.3.9',
+                '9.3.10', '9.4.1', '9.4.4', '9.4.5']
 
     def get_db_families(self):
         return ["postgres9.3", "postgres9.4"]

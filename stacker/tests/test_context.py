fa1444abb7a780e619908954b035f71b68112b5d
Olivier Van Goethem <olivier.van-goethem@cloudreach.com>
2016-06-27 16:45:58 +0100

    Removing duplicate tests

--- stacker/tests/test_context.py
+++ stacker/tests/test_context.py
@@ -97,23 +97,6 @@ class TestContext(unittest.TestCase):
         context.load_config("""stacker_bucket_sse: blah""")
         self.assertEqual(context.bucket_sse, False)
 
-    def test_context_default_bucket_server_side_encryption(self):
-        context = Context({"namespace": "test"})
-        context.load_config("""mappings:""")
-        self.assertEqual(context.bucket_sse, False)
-
-    def test_context_bucket_server_side_encryption_true(self):
-        context = Context({"namespace": "test"})
-        context.load_config("""stacker_bucket_sse: true""")
-        self.assertEqual(context.bucket_sse, True)
-
-    def test_context_bucket_server_side_encryption_other_reverts_to_default(
-            self
-    ):
-        context = Context({"namespace": "test"})
-        context.load_config("""stacker_bucket_sse: blah""")
-        self.assertEqual(context.bucket_sse, False)
-
 
 class TestFunctions(unittest.TestCase):
     """ Test the module level functions """

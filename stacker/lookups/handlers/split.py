7e5fea2eab13d30e0406e7fc30c392618f0d68aa
Michael Barrett <loki77@gmail.com>
2017-01-23 12:48:50 -0800

    New split handler for splitting values

---
+++ stacker/lookups/handlers/split.py
@@ -0,0 +1,33 @@
+TYPE_NAME = "split"
+
+
+def handler(value, **kwargs):
+    """Split the supplied string on the given delimiter, providing a list.
+
+    Format of value:
+
+        <delimiter>::<value>
+
+    For example:
+
+        Subnets: ${split ,::subnet-1,subnet-2,subnet-3}
+
+    Would result in the variable `Subnets` getting a list consisting of:
+
+        ["subnet-1", "subnet-2", "subnet-3"]
+
+    This is particularly useful when getting an output from another stack that
+    contains a list. For example, the standard vpc blueprint outputs the list
+    of Subnets it creates as a pair of Outputs (PublicSubnets, PrivateSubnets)
+    that are comma separated, so you could use this in your config:
+
+        Subnets: ${split ,::${output vpc::PrivateSubnets}}
+    """
+
+    try:
+        delimiter, text = value.split("::", 1)
+    except ValueError:
+        raise ValueError("Invalid value for split: %s. Must be in "
+                         "<delimiter>::<text> format." % value)
+
+    return text.split(delimiter)

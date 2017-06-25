1c1ea59b496ae3c7a41354aae27c0742d585d15f
Jesse Peters <syphon7@gmail.coim>
2017-06-25 13:14:52 -0500

    Added a lookup for system environment variables.

---
+++ stacker/lookups/handlers/envvar.py
@@ -0,0 +1,35 @@
+import os
+
+from ...util import read_value_from_path
+
+TYPE_NAME = "envvar"
+
+
+def handler(value, **kwargs):
+    """Retrieve an environment variable.
+
+    For example:
+
+        # In stacker we would reference the environment variable like this:
+        conf_key: ${envvar ENV_VAR_NAME}
+
+        You can optionally store the value in a file, ie:
+
+        envvar_value.txt
+        ENV_VAR_NAME
+
+        and reference it within stacker (NOTE: the path should be relative to
+        the stacker config file):
+
+        conf_key: ${envvar file://envvar_value.txt}
+
+        # Both of the above would resolve to
+        conf_key: ENV_VALUE
+
+    """
+    value = read_value_from_path(value)
+
+    if value in os.environ:
+        return os.environ[value]
+    else:
+        raise ValueError('EnvVar "{}" does not exist'.format(value))

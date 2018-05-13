cloudtools/stacker

commit 4c58c0b11237b808e9ad1d045ac5f50d1b928405

Run dump and outline actions offline (#594)
Build does not need access to AWS when run with `dump` or
`outline` options so make it run offline for those actions.

stacker/actions/build.py
@@ -72,6 +72,20 @@ def should_submit(stack):
     return False
 
 
+def should_ensure_cfn_bucket(outline, dump):
+    """Test whether access to the cloudformation template bucket is required
+
+    Args:
+        outline (bool): The outline action.
+        dump (bool): The dump action.
+
+    Returns:
+        bool: If access to CF bucket is needed, return True.
+
+    """
+    return outline is False and dump is False
+
+
 def _resolve_parameters(parameters, blueprint):
     """Resolves CloudFormation Parameters for a given blueprint.
 
@@ -351,7 +365,8 @@ class Action(BaseAction):
 
     def pre_run(self, outline=False, dump=False, *args, **kwargs):
         """Any steps that need to be taken prior to running the action."""
-        self.ensure_cfn_bucket()
+        if should_ensure_cfn_bucket(outline, dump):
+            self.ensure_cfn_bucket()
         hooks = self.context.config.pre_build
         handle_hooks(
             "pre_build",

stacker/tests/actions/test_build.py
@@ -155,6 +155,26 @@ class TestBuildAction(unittest.TestCase):
             mock_stack.force = t.force
             self.assertEqual(build.should_update(mock_stack), t.result)
 
+    def test_should_ensure_cfn_bucket(self):
+        test_scenarios = [
+            {"outline": False, "dump": False, "result": True},
+            {"outline": True, "dump": False, "result": False},
+            {"outline": False, "dump": True, "result": False},
+            {"outline": True, "dump": True, "result": False},
+            {"outline": True, "dump": "DUMP", "result": False}
+        ]
+
+        for scenario in test_scenarios:
+            outline = scenario["outline"]
+            dump = scenario["dump"]
+            result = scenario["result"]
+            try:
+                self.assertEqual(
+                    build.should_ensure_cfn_bucket(outline, dump), result)
+            except AssertionError as e:
+                e.args += ("scenario", str(scenario))
+                raise
+
     def test_should_submit(self):
         test_scenario = namedtuple("test_scenario",
                                    ["enabled", "result"])

commit ac4e3a0bd4aa90262d46eb8a5302e8dae5657a0b

Fix an issue introduced in #594
594 assumed that `dump` would use `False` as it's default, but since
`--dump` is a `str` type, it instead uses `None` when it is not
specified on the CLI.

The truth is that this is more pythonic anyway, and has the same effect.
I can't imagine when this could cause un-expected behaivior.

stacker/actions/build.py
@@ -83,7 +83,8 @@ def should_ensure_cfn_bucket(outline, dump):
         bool: If access to CF bucket is needed, return True.
 
     """
-    return outline is False and dump is False
+    print "OUTLINE: %s, DUMP: %s" % (outline, dump)
+    return not outline and not dump
 
 
 def _resolve_parameters(parameters, blueprint):
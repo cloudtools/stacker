d639334a82d4cc6c51979d4a3ce6564b361d9691
Kai Xia <xiaket@gmail.com>
2018-05-19 21:04:04 +1000

    fix lint issues.
    Signed-off-by: Kai Xia <xiaket@gmail.com>

--- stacker/exceptions.py
+++ stacker/exceptions.py
@@ -1,6 +1,8 @@
 from __future__ import print_function
 from __future__ import division
 from __future__ import absolute_import
+
+
 class InvalidConfig(Exception):
     def __init__(self, errors):
         super(InvalidConfig, self).__init__(errors)
--- stacker/providers/base.py
+++ stacker/providers/base.py
@@ -1,6 +1,8 @@
 from __future__ import print_function
 from __future__ import division
 from __future__ import absolute_import
+
+
 def not_implemented(method):
     raise NotImplementedError("Provider does not support '%s' "
                               "method." % method)

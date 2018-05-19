31b95baca076c68c544147745b0785aa2e0081b0
Kai Xia <xiaket@gmail.com>
2018-05-20 01:15:48 +1000

    fix encoding issue in kms.
    nose2: 1 failure
    nose3: 4 failures
    
    Signed-off-by: Kai Xia <xiaket@gmail.com>

--- stacker/tests/lookups/handlers/test_kms.py
+++ stacker/tests/lookups/handlers/test_kms.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 from __future__ import division
 from __future__ import absolute_import
+import codecs
 import unittest
 
 from moto import mock_kms
@@ -12,13 +13,15 @@ from stacker.lookups.handlers.kms import handler
 
 class TestKMSHandler(unittest.TestCase):
     def setUp(self):
-        self.plain = "my secret"
+        self.plain = b"my secret"
         with mock_kms():
             kms = boto3.client("kms", region_name="us-east-1")
             self.secret = kms.encrypt(
                 KeyId="alias/stacker",
-                Plaintext=self.plain.encode("base64")
+                Plaintext=codecs.encode(self.plain, 'base64').decode('utf-8'),
             )["CiphertextBlob"]
+            if isinstance(self.secret, bytes):
+                self.secret = self.secret.decode()
 
     def test_kms_handler(self):
         with mock_kms():

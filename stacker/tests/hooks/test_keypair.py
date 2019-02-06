e30ca6bcfd478f92d54c9a9951f8e8909ed1f556
Alec Rajeev <arajeev@onica.com>
2019-02-06 14:10:14 -0600

    Added complete unit test with log veriying

--- stacker/tests/hooks/test_keypair.py
+++ stacker/tests/hooks/test_keypair.py
@@ -17,6 +17,7 @@ from ..factories import (
 )
 
 REGION = "us-east-1"
+KEY_PAIR_NAME = "FakeKey"
 
 
 class TestKeypairHooks(unittest.TestCase):
@@ -26,33 +27,21 @@ class TestKeypairHooks(unittest.TestCase):
         self.context = mock_context(namespace="fake")
 
     @patch("stacker.hooks.keypair.input", create=True)
-    def test_keypair_missing(self, mocked_input):
+    def test_keypair_missing_cancel_input(self, mocked_input):
         mocked_input.side_effect = ["Cancel"]
         with mock_ec2():
-            result = ensure_keypair_exists(provider=self.provider, context=self.context, keypair="FakeKey")
-            self.assertEqual(result, False)  # test1
-            #     logger = "stacker.hooks.ec2"
-            #     client = boto3.client("ec2", region_name=REGION)
-            #     response = client.describe_key_pairs()
+            logger = "stacker.hooks.ec2"
+            client = boto3.client("ec2", region_name=REGION)
+            response = client.describe_key_pairs()
 
-            #     self.assertEqual(len(response["KeyPairs"]), 0)
-            #     value = ensure_keypair_exists(provider=self.provider, context=self.context, keypair="FakeKey")
-            #     self.assertEqual(value, False)
-
-                # with LogCapture(logger) as logs:
-                #     ensure_keypair_exists(
-                #         provider=self.provider,
-                #         context=self.context,
-                #         keypair="FakeKey"
-                #     )
-
-                #     logs.check(
-                #         (
-                #             logger,
-                #             "info",
-                #             "keypair: \"%s\" not found", "FakeKey"
-                #         )
-                #     )
-
-                # response = client.describe_key_pairs()
-                # self.assertEqual(len(response["KeyPairs"]), 0)
+            # initially no key pairs created
+            self.assertEqual(len(response["KeyPairs"]), 0)
+            with LogCapture(logger) as logs:
+                self.assertFalse(ensure_keypair_exists(provider=self.provider, context=self.context, keypair="FakeKey"))
+                logs.check(
+                    (
+                        logger,
+                        "info",
+                        "keypair: \"%s\" not found", KEY_PAIR_NAME
+                    )
+                )

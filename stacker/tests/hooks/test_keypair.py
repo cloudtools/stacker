dd49e143fa77e7315485640881bea679474584af
Alec Rajeev <arajeev@onica.com>
2019-02-06 14:59:11 -0600

    fixed lint

--- stacker/tests/hooks/test_keypair.py
+++ stacker/tests/hooks/test_keypair.py
@@ -64,13 +64,16 @@ class TestKeypairHooks(unittest.TestCase):
             keypair = find(response["KeyPairs"], "KeyName", KEY_PAIR_NAME)
             with LogCapture(logger) as logs:
                 value = ensure_keypair_exists(provider=self.provider,
-                                                       context=self.context,
-                                                       keypair=KEY_PAIR_NAME)
+                                                context=self.context,
+                                                keypair=KEY_PAIR_NAME)
                 logs.check(
                     (
                         logger,
                         "INFO",
-                        "keypair: %s (%s) %s" % KEY_PAIR_NAME, keypair["KeyFingerprint"], "exists"
+                        "keypair: %s (%s) %s" %
+                        KEY_PAIR_NAME,
+                        keypair["KeyFingerprint"],
+                        "exists"
                     )
                 )
                 print(value)

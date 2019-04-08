943acbfa0cf5eaf51e607acda332eee87cadae58
Michael Barrett <loki77@gmail.com>
2019-04-08 15:09:29 -0700

    Fix lint issues

--- stacker/tests/test_util.py
+++ stacker/tests/test_util.py
@@ -31,13 +31,6 @@ from stacker.util import (
     SourceProcessor
 )
 
-from stacker.hooks.utils import handle_hooks
-
-from .factories import (
-    mock_context,
-    mock_provider,
-)
-
 regions = ["us-east-1", "cn-north-1", "ap-northeast-1", "eu-west-1",
            "ap-southeast-1", "ap-southeast-2", "us-west-2", "us-gov-west-1",
            "us-west-1", "eu-central-1", "sa-east-1"]

dd04d277216668c45e25803c1e509bb88c331569
Kyle Finley <kyle@finley.sh>
2019-11-27 14:40:14 -0800

    refactor kms lookup unit test to use stubber
    it was failing when using moto to mock the calls. made an attempt to resolve with the moto test but was unable to fix it quickly.

--- stacker/tests/lookups/handlers/test_kms.py
+++ stacker/tests/lookups/handlers/test_kms.py
@@ -2,35 +2,51 @@ from __future__ import print_function
 from __future__ import division
 from __future__ import absolute_import
 import codecs
+from mock import patch
 import unittest
 
-from moto import mock_kms
-
 import boto3
+from botocore.stub import Stubber
 
 from stacker.lookups.handlers.kms import KmsLookup
+from stacker.tests.factories import SessionStub, mock_provider
+
+
+REGION = 'us-east-1'
 
 
 class TestKMSHandler(unittest.TestCase):
+    client = boto3.client('kms', region_name=REGION)
+
     def setUp(self):
-        self.plain = b"my secret"
-        with mock_kms():
-            kms = boto3.client("kms", region_name="us-east-1")
-            self.secret = kms.encrypt(
-                KeyId="alias/stacker",
-                Plaintext=codecs.encode(self.plain, 'base64').decode('utf-8'),
-            )["CiphertextBlob"]
-            if isinstance(self.secret, bytes):
-                self.secret = self.secret.decode()
+        self.stubber = Stubber(self.client)
+        self.provider = mock_provider(region=REGION)
+        self.secret = b'my secret'
 
-    def test_kms_handler(self):
-        with mock_kms():
-            decrypted = KmsLookup.handle(self.secret)
-            self.assertEqual(decrypted, self.plain)
+    @patch("stacker.lookups.handlers.kms.get_session",
+           return_value=SessionStub(client))
+    def test_kms_handler(self, _mock_client):
+        self.stubber.add_response('decrypt', {'Plaintext': self.secret},
+                                  {'CiphertextBlob': codecs.decode(self.secret,
+                                                                   'base64')})
 
-    def test_kms_handler_with_region(self):
-        region = "us-east-1"
-        value = "%s@%s" % (region, self.secret)
-        with mock_kms():
-            decrypted = KmsLookup.handle(value)
-            self.assertEqual(decrypted, self.plain)
+        with self.stubber:
+            self.assertEqual(self.secret,
+                             KmsLookup.handle(value=self.secret.decode(),
+                                              provider=self.provider))
+            self.stubber.assert_no_pending_responses()
+
+    @patch("stacker.lookups.handlers.kms.get_session",
+           return_value=SessionStub(client))
+    def test_kms_handler_with_region(self, _mock_client):
+        value = '{}@{}'.format(REGION, self.secret.decode())
+
+        self.stubber.add_response('decrypt', {'Plaintext': self.secret},
+                                  {'CiphertextBlob': codecs.decode(self.secret,
+                                                                   'base64')})
+
+        with self.stubber:
+            self.assertEqual(self.secret,
+                             KmsLookup.handle(value=value,
+                                              provider=self.provider))
+            self.stubber.assert_no_pending_responses()

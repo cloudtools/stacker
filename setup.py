1284b3772d76234dc4ba6a0a636a8c4f0c177d46
Till Heistermann <till.heistermann@moia.io>
2022-02-03 23:49:52 +0100

    Relax requirements that were pinned down because they no longer supported python 2

--- setup.py
+++ setup.py
@@ -5,34 +5,15 @@ VERSION = "1.7.2"
 
 src_dir = os.path.dirname(__file__)
 
-install_requires = [
-    "future",
-    "troposphere>=1.9.0",
-    'botocore>=1.12.111',  # matching boto3 requirement
-    "boto3>=1.9.111,<2.0",
-    "PyYAML>=3.13b1",
-    "awacs>=0.6.0",
-    "gitpython>=2.0,<3.0",
-    "jinja2>=2.7,<3.0a",
-    "schematics>=2.0.1,<2.1.0",
-    "formic2",
-    "python-dateutil>=2.0,<3.0",
-    "MarkupSafe<2.0", # 2.0 dropped python 2.7, 3.5 support - temporary
-    "more-itertools<6.0.0", # 6.0.0 dropped python 2.7 support - temporary
-    "rsa==4.5", # 4.6 dropped python 2.7 support - temporary
-    "python-jose<3.2.0", # 3.2.0 dropped python 2.7 support - temporary
-]
+def get_install_requirements(path):
+    content = open(os.path.join(os.path.dirname(__file__), path)).read()
+    return [req for req in content.split("\n") if req != "" and not req.startswith("#")]
+
+install_requires = get_install_requirements("requirements.in")
 
 setup_requires = ['pytest-runner']
 
-tests_require = [
-    "pytest~=4.3",
-    "pytest-cov~=2.6",
-    "mock~=2.0",
-    "moto[awslambda]~=1.3.16",
-    "testfixtures~=4.10.0",
-    "flake8-future-import",
-]
+tests_require = get_install_requirements("test-requirements.in")
 
 scripts = [
     "scripts/compare_env",
@@ -68,8 +49,9 @@ if __name__ == "__main__":
             "Development Status :: 5 - Production/Stable",
             "Environment :: Console",
             "License :: OSI Approved :: BSD License",
-            "Programming Language :: Python :: 2.7",
-            "Programming Language :: Python :: 3.5",
-            "Programming Language :: Python :: 3.6",
+            "Programming Language :: Python :: 3.7",
+            "Programming Language :: Python :: 3.8",
+            "Programming Language :: Python :: 3.9",
+            "Programming Language :: Python :: 3.10",
         ],
     )
